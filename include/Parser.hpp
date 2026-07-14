a2859a14a12d2f710253ea8333642ef3309a84e0
Joshua Wang <joshuawang9999@gmail.com>
2026-07-13 21:33:06 -0400

    parse hpp file:

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -7,7 +7,7 @@ class Parser
 {
 public:
     explicit Parser(const std::vector<Token> &tokens);
-    Expr *parse();
+    std::unique_ptr<Expr> parse();
 
 private:
     const std::vector<Token> &tokens;
@@ -18,4 +18,12 @@ private:
     bool match(TokenType type);
     bool check(TokenType type);
     Token consume(TokenType type, const std::string &message);
+
+    std::unique_ptr<Expr> expression();
+    std::unique_ptr<Expr> equality();
+    std::unique_ptr<Expr> comparison();
+    std::unique_ptr<Expr> term();
+    std::unique_ptr<Expr> factor();
+    std::unique_ptr<Expr> unary();
+    std::unique_ptr<Expr> primary();
 };
