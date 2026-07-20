fafefc9014c61f1bfecfffc32386cc114e78fad4
Joshua Wang <joshuawang9999@gmail.com>
2026-07-19 21:36:52 -0400

    implemented varstatement and printstatement

--- include/Statements/PrintStatement.hpp
+++ include/Statements/PrintStatement.hpp
@@ -3,4 +3,12 @@
 class PrintStatement : public Statement
 {
 public:
+    std::unique_ptr<Expr> expression;
+
+    PrintStatement(std::unique_ptr<Expr> expression) : expression(std::move(expression)) {}
+
+    void accept(StatementVisitor &visitor) override
+    {
+        visitor.visitPrintStatement(*this);
+    }
 };
--- include/Statements/VarStatement.hpp
+++ include/Statements/VarStatement.hpp
@@ -3,4 +3,13 @@
 class VarStatement : public Statement
 {
 public:
+    const std::string name;
+    std::unique_ptr<Expr> initializer;
+
+    VarStatement(std::string name, std::unique_ptr<Expr> initializer) : name(std::move(name)), initializer(std::move(initializer)) {}
+
+    void accept(VarVisitor &visitor) override
+    {
+        visitor.visitVarStatement(*this);
+    }
 };
